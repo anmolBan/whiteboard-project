b5abc69901c2eb54fe25c75093752bb04d7e2bfc
Anmol Bansal <bansal.anmol98@gmail.com>
2026-04-25 01:23:24 +0530

    update

--- apps/white-board/next.config.ts
+++ apps/white-board/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  transpilePackages: ["@repo/db", "@repo/types"]
 };
 
 export default nextConfig;
